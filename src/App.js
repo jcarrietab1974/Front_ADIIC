e32c42bdd9dccaddaa8e6acaee99b1c1e9a133e8
juan arrieta <caporo1974@yahoo.es>
2024-08-21 18:44:48 -0500

    Se instala el framework tailwindcss, al instalarse se crean la carpetas tailwind.config.js y postcss.config.js, se configuran las rutas para consumir tailwindcss en el archivo tailwind.config.js, se agregan las directivas para cada una de las capas de tailwindcss en el archivo index.css en la carpeta src, se crea el formulario de iniciar sesión en el archivo login.js y el formulario de registro en el archivo crearCuenta.js que se encuentra en la carpeta componentes la cual se encuentra en la carpeta src

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from "react";
-<<<<<<< HEAD
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-=======
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
->>>>>>> 63147b37533bdd16aa1e591ed6fe4ebf80d87542
 import Login from "./componentes/Login";
 import CrearCuenta from "./componentes/CrearCuenta";
 
 function App() {
   return (
-<<<<<<< HEAD
-   <Router>
-     <Routes>
-       <Route path="/" exact element = {<Login/>}/>
-       <Route path="/crear-cuenta" exact element = {<CrearCuenta/>}/>
-
-     </Routes>
-   </Router>
-=======
     <Router>
       <Routes>
         <Route path="/" exact element={<Login />} />
         <Route path="/crear-cuenta" exact element={<CrearCuenta />} />
       </Routes>
     </Router>
->>>>>>> 63147b37533bdd16aa1e591ed6fe4ebf80d87542
   );
 }
 export default App;
--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,77 +1,59 @@
 import React from "react";
-<<<<<<< HEAD
 // import { Link } from "react-router-dom";
-import { Link } from 'react-router-dom';
-
-const Login = () => {
-    return (
-      <main className='container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center'>
-      <div className='md:w-2/3 lg:w-2/5'>
-         <h1 className=' colum bg-gradient-to-r from-indigo-200 via-violet-700 to-indigo-200
-         bg-clip-text font-display text-4xl tracking-tight text-transparent '>
-           ADIIC Dotaciones Institucionales
-         </h1>
-
-         <h2 className='colum bg-gradient-to-r from-indigo-200 via-violet-700 to-indigo-200
-         bg-clip-text font-display text-4xl tracking-tight text-transparent text-center'>Iniciar sesion</h2>
-   
-         <form className="my-10 bg-white shadow rounded-lg p-10">
-           <div className="my-5">
-             <label className="uppercase text-gray-600 block text-lx font-bold">Email</label>
-             <input
-               type="email"
-               placeholder="Email de registro"
-               className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-             />
-                     <br></br>
-             <label className="uppercase text-gray-600 block text-lx font-bold">Password</label>
-             <input
-               type="password"
-               placeholder="Password de registro"
-               className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-             />
-        
-           </div>
-
-           <input
-                  type="submit"
-                  value="Iniciar Sesión"
-                  className="bg-violet-600 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer"
-             />
-           
-
-              <Link
-                className="block text-center my-5"
-                to={"/crear-cuenta"}
-              >           
-                Crear Cuenta
-             </Link>
-         </form>
-
-      </div>
-
-   </main>
-      
-    );
-}
-
-export default Login;
-=======
-import { Link } from "react-router-dom";    
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Almacenes "ADIIC Dotaciones Institucionales"</h1>
-      <h1>Iniciar sesión</h1>
-      <h2>Bienvenido, ingrese sus credenciales</h2>
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button>Ingresar</button>
-      <Link to={"/crear-cuenta"}>Crear cuenta</Link>
-    </div>
+    <main className="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center">
+      <div className="md:w-2/3 lg:w-2/5">
+        <h1
+          className=" colum bg-gradient-to-r from-indigo-200 via-violet-700 to-indigo-200
+         bg-clip-text font-display text-4xl tracking-tight text-transparent "
+        >
+          ADIIC Dotaciones Institucionales
+        </h1>
+
+        <h2
+          className="colum bg-gradient-to-r from-indigo-200 via-violet-700 to-indigo-200
+         bg-clip-text font-display text-4xl tracking-tight text-transparent text-center"
+        >
+          Iniciar sesion
+        </h2>
+
+        <form className="my-10 bg-white shadow rounded-lg p-10">
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-lx font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email de registro"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            />
+            <br></br>
+            <label className="uppercase text-gray-600 block text-lx font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password de registro"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Iniciar Sesión"
+            className="bg-violet-600 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer"
+          />
+
+          <Link className="block text-center my-5" to={"/crear-cuenta"}>
+            Crear Cuenta
+          </Link>
+        </form>
+      </div>
+    </main>
   );
 };
 
 export default Login;
->>>>>>> 63147b37533bdd16aa1e591ed6fe4ebf80d87542
